fe7c47f4dd46c55bdcba9fbf75bbd4739f936699
Saithirius <look1899@mail.ru>
2020-07-08 03:17:33 +0500

    add scroll animation for examples

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,22 +1,24 @@
+// Анимация появления фотографии и данных
 $(window).on('load', () => {
   $('.photo').find('img').animate({opacity: 1}, 1000);
   $('.data').animate({left: 0}, 700);
 });
 // Анимация появления примеров 
-$(window).scroll(function() {
+$(window).scroll(() => {
   if($(window).height() + $(window).scrollTop() > $('.examples__header').offset().top){
     $('.examples__blocks').addClass('animate');
   }
 })
-
+// Анимация скроллинга примеров
+const el = $('.examples');
+$(window).scroll(() => {
+  const percentHeigtEl = el.outerHeight() / 100;
+  const winScr = $(window).height() + $(window).scrollTop() - el.offset().top - 50;
+  const scrollPercent = winScr / percentHeigtEl;
+  $('.scroll').css('width', scrollPercent + '%');
+});
 // Плавная прокрутка
-$("body").on('click', '[href*="#"]', function(e){
+$("body").on('click', '[href*="#"]', e => {
   $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - 50 }, 1000);
   e.preventDefault();
 });
-
-// //Прелоудер
-// $(window).on('load', () => {
-//   $('.preloader').addClass('loaded');
-//   window.setTimeout(() => {$('.preloader').css('display', 'none');}, 800);
-// });	
